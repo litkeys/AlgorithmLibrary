606ba213e15d4db56a0cf4284959ac913603c8b1
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-04-21 14:17:41 +1000

    Updated srange function

--- Algorithms Library/Smart Range.py
+++ Algorithms Library/Smart Range.py
@@ -1,20 +1,26 @@
-def srange(*args):
+# smart range function that automatically negates the step argument when start is more positive than stop
+def srange(*args: int):
     l = len(args)
-    if l == 1:
+    if l == 1: # srange(stop)
         start = 0
         stop = args[0]
         step = 1
-    elif l == 2:
+    elif l == 2: # srange(start, stop)
         start = args[0]
         stop = args[1]
         step = 1
-    elif l == 3:
+    elif l == 3: # srange(start, stop, step)
         start = args[0]
         stop = args[1]
-        step = args[2]
+        if args[2] != 0:
+            step = args[2]
+        else:
+            raise ValueError("srange() arg 3 must not be zero")
     else:
         raise TypeError(f"srange expected at most 3 arguments, got {l}")
 
     if start > stop:
-        return range(start, stop, -step)
+        if step > 0:
+            return range(start, stop, -step)
+        return range(start, stop, step)
     return range(start, stop, step)
