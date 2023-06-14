933a7b4f0bb64fb308a52d3ff1d2fe7a39554cf9
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-06-14 20:14:34 +1000

    srange function now supports float steps, and returns its own generator

--- Algorithms Library/Smart Range.py
+++ Algorithms Library/Smart Range.py
@@ -1,6 +1,29 @@
-# smart range function that automatically negates the step argument when start is more positive than stop
-def srange(*args: list[int]):
+def srange(*args: list[int]): # returns a generator
+
+    """
+    smart range function that supports negative and float steps
+    if start is larger or more positive than stop, then step will automatically be negated
+    float steps work intuitively as one would expect
+
+    all input arguments should be integers and floats
+    however if a type mismatch happens, the function will try to convert unknown type into float or int
+    examples of such include an int/float in string, Decimal class, etc.
+
+    the default value for start is 0, and for step is 1 (or -1 if start > stop)
+    """
+
     l = len(args)
+
+    for i in range(l):
+        t = type(args[i])
+        if t != int and t != float:
+            try: # try to convert unknown argument type into floats, then ints if applicable
+                args[i] = float(args[i])
+                if args[i] % 1 == 0:
+                    args[i] = int(args[i])
+            except Exception:
+                raise ValueError(f"srange argument {i} must be of type int or float, not {t}")
+
     if l == 1: # srange(stop)
         start = 0
         stop = args[0]
@@ -15,12 +38,23 @@ def srange(*args: list[int]):
         if args[2] != 0:
             step = args[2]
         else:
-            raise ValueError("srange() arg 3 must not be zero")
+            raise ValueError("srange arg 3 must not be zero")
     else:
-        raise TypeError(f"srange expected at most 3 arguments, got {l}")
+        raise Exception(f"srange expected at most 3 arguments, got {l}")
+    
+    # at this point, start, stop, step are all defined
 
     if start > stop:
         if step > 0:
-            return range(start, stop, -step)
-        return range(start, stop, step)
-    return range(start, stop, step)
+            step = -step
+            
+    current_value = start
+
+    if start < stop:
+        while current_value < stop:
+            yield current_value
+            current_value += step
+    else:
+        while current_value > stop:
+            yield current_value
+            current_value += step
