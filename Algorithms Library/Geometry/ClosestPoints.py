75232a2ec52ee6d810be2d43c40392e6e884427b
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-07-02 13:43:30 +1000

    Implemented optimised closest points algorithm

--- Algorithms Library/Geometry/ClosestPoints.py
+++ Algorithms Library/Geometry/ClosestPoints.py
@@ -1,143 +1,135 @@
 """
-The algorithm finds distance between closest pair of points
-in the given n points.
-Approach used -> Divide and conquer
-The points are sorted based on Xco-ords and
-then based on Yco-ords separately.
-And by applying divide and conquer approach,
-minimum distance is obtained recursively.
+Given a set of points, find the closest pair.
 
->> Closest points can lie on different sides of partition.
-This case handled by forming a strip of points
-whose Xco-ords distance is less than closest_pair_dis
-from mid-point's Xco-ords. Points sorted based on Yco-ords
-are used in this step to reduce sorting time.
-Closest pair distance is found in the strip of points. (closest_in_strip)
+The algorithm uses a divide-and-conquer strategy
+that splits the points into 2 regions recursively.
+The solution lies either in the left region,
+the right region, or between the 2 regions.
 
-min(closest_pair_dis, closest_in_strip) would be the final answer.
+Therefore, there are three steps in total in
+each recursive call:
+- Calculate solution to the left region
+- Calculate solution to the right region
+- Calculate solution to the middle 'strip'
 
-Time complexity: O(n * log n)
+It is proven mathematically that calculating
+the solution to the middle 'strip' takes only
+O(n), see link below for further explanation:
+https://www.cs.cmu.edu/~15451-s20/lectures/lec23-closest-pair.pdf
+
+Therefore, the total time & space complexity is O(n log n)
 """
 
-
-def euclidean_distance_sqr(point1, point2):
+def distance_euclidean(point1: tuple[float, float], point2: tuple[float, float]) -> float:
     """
-    >>> euclidean_distance_sqr([1,2],[2,4])
-    5
+    >>> print(distance_euclidean((0, 0), (3, 4)))
+    5.0
     """
-    return (point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**0.5
 
+### Temporarily unneeded ###
+# def distance_manhattan(point1: tuple[int, int], point2: tuple[int, int]) -> int:
+#     """
+#     >>> print(distance_manhattan((0, 0), (3, 4)))
+#     7
+#     """
+#     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
-def column_based_sort(array, column=0):
+def bruteforce_closest_pair(points: list[tuple[float, float]], min_distance = float("inf")) -> list[tuple[float, float]]:
     """
-    >>> column_based_sort([(5, 1), (4, 2), (3, 0)], 1)
-    [(3, 0), (5, 1), (4, 2)]
+    Brute force O(n^2) algorithm to find the closest pair of points
+    Only called when input size <= 3
+    NOTE: Unless intended, do not call this function manually, as it is a helper function
     """
-    return sorted(array, key=lambda x: x[column])
+    closest_pair = None
+    l = len(points)
+    for i in range(l-1): # pick point 1
+        for j in range(i+1, l): # pick point 2
+            distance = distance_euclidean(points[i], points[j])
+            if distance < min_distance:
+                min_distance = distance
+                closest_pair = [points[i], points[j]]
+    return closest_pair
 
-
-def dis_between_closest_pair(points, points_counts, min_dis=float("inf")):
+def _closest_pair_in_strip(points: list[tuple[float, float]], min_distance = float("inf")) -> list[tuple[float, float]]:
     """
-    brute force approach to find distance between closest pair points
-
-    Parameters :
-    points, points_count, min_dis (list(tuple(int, int)), int, int)
-
-    Returns :
-    min_dis (float):  distance between closest pair of points
-
-    >>> dis_between_closest_pair([[1,2],[2,4],[5,7],[8,9],[11,0]],5)
-    5
-
+    Linear O(n) algorithm to find the closest pair of points in a y-sorted 'strip'
+    Each inner loop runs in O(1) as at most 3 other points besides or below are checked
+    This is because the mth point belongs to the right section and defines the middle line
+    Therefore there is no need to check the right half of the 'strip'
+    This results in only 3 such squares needing to be checked, see illustration in link at the top
+    NOTE: DO NOT call this function manually, as it is a helper function
     """
+    closest_pair = None
+    l = len(points)
+    for i in range(1, l): # pick point 1
+        for j in range(max(0, i-3), i): # pick point 2
+            distance = distance_euclidean(points[i], points[j])
+            if distance < min_distance:
+                min_distance = distance
+                closest_pair = [points[i], points[j]]            
+    return closest_pair
 
-    for i in range(points_counts - 1):
-        for j in range(i + 1, points_counts):
-            current_dis = euclidean_distance_sqr(points[i], points[j])
-            if current_dis < min_dis:
-                min_dis = current_dis
-    return min_dis
-
-
-def dis_between_closest_in_strip(points, points_counts, min_dis=float("inf")):
+def _closest_pair_of_points(points_sorted_on_x: list[tuple[float, float]], points_sorted_on_y: list[tuple[float, float]], points_count: int) -> list[tuple[float, float]]:
     """
-    closest pair of points in strip
-
-    Parameters :
-    points, points_count, min_dis (list(tuple(int, int)), int, int)
-
-    Returns :
-    min_dis (float):  distance btw closest pair of points in the strip (< min_dis)
-
-    >>> dis_between_closest_in_strip([[1,2],[2,4],[5,7],[8,9],[11,0]],5)
-    85
-    """
-
-    for i in range(min(6, points_counts - 1), points_counts):
-        for j in range(max(0, i - 6), i):
-            current_dis = euclidean_distance_sqr(points[i], points[j])
-            if current_dis < min_dis:
-                min_dis = current_dis
-    return min_dis
-
-
-def closest_pair_of_points_sqr(points_sorted_on_x, points_sorted_on_y, points_counts):
-    """divide and conquer approach
-
-    Parameters :
-    points, points_count (list(tuple(int, int)), int)
-
-    Returns :
-    (float):  distance btw closest pair of points
-
-    >>> closest_pair_of_points_sqr([(1, 2), (3, 4)], [(5, 6), (7, 8)], 2)
-    8
+    Efficient O(n log n) algorithm for finding the closest pair of points given a sorted list of points
+    To find the solution to the left & right regions, points_sorted_on_x are used
+    To find the solution to the middle 'strip', points_sorted_on_y are used
+    NOTE: DO NOT call this function manually, as it is a helper function
     """
 
-    # base case
-    if points_counts <= 3:
-        return dis_between_closest_pair(points_sorted_on_x, points_counts)
+    # base case, bruteforce-able as it cannot be further divided
+    if points_count <= 3:
+        return bruteforce_closest_pair(points_sorted_on_x)
+    
+    # divide and conquer with recursion for left and right regions
+    m = points_count//2
+    left_closest_pair = _closest_pair_of_points(points_sorted_on_x[:m], points_sorted_on_y[:m], m)
+    left_distance = distance_euclidean(*left_closest_pair)
+    right_closest_pair = _closest_pair_of_points(points_sorted_on_x[m:], points_sorted_on_y[m:], points_count-m)
+    right_distance = distance_euclidean(*right_closest_pair)
+    if left_distance < right_distance:
+        closest_distance = left_distance
+        closest_pair = left_closest_pair
+    elif right_distance < left_distance:
+        closest_distance = right_distance
+        closest_pair = right_closest_pair
+    else: # this is the default solution, which may need to be modified for specific problem requirements
+        closest_distance = left_distance
+        closest_pair = left_closest_pair
+    
+    # middle 'strip' calculation, optimised by ignoring the right half which includes the mth point
+    points_in_strip = []
+    for point in points_sorted_on_y: # only points from the left region are considered
+        if point[0] <= points_sorted_on_x[m][0] and distance_euclidean(point, points_sorted_on_x[m]) < closest_distance:
+            points_in_strip.append(point)
+    strip_closest_pair = _closest_pair_in_strip(points_in_strip, closest_distance)
+    if strip_closest_pair != None:
+        strip_distance = distance_euclidean(*strip_closest_pair)
+    else:
+        strip_distance = float("inf")
 
-    # recursion
-    mid = points_counts // 2
-    closest_in_left = closest_pair_of_points_sqr(
-        points_sorted_on_x, points_sorted_on_y[:mid], mid
-    )
-    closest_in_right = closest_pair_of_points_sqr(
-        points_sorted_on_y, points_sorted_on_y[mid:], points_counts - mid
-    )
-    closest_pair_dis = min(closest_in_left, closest_in_right)
+    # final solution
+    if closest_distance < strip_distance:
+        return closest_pair
+    elif strip_distance < closest_distance:
+        return strip_closest_pair
+    else: # this is the default solution, which may need to be modified for specific problem requirements
+        return closest_pair
 
+def closest_pair_of_points(points: list[tuple[float, float]]):
     """
-    cross_strip contains the points, whose Xcoords are at a
-    distance(< closest_pair_dis) from mid's Xcoord
+    Each point must be represented by a tuple (x, y)
+    NOTE: Call this function to get the closest pair of points in an arbitrarily ordered set of points
     """
+    points_count = len(points)
+    points_sorted_on_x = sorted(points, key = lambda p: p[0])
+    points_sorted_on_y = sorted(points, key = lambda p: p[1])
+    return _closest_pair_of_points(points_sorted_on_x, points_sorted_on_y, points_count)
 
-    cross_strip = []
-    for point in points_sorted_on_x:
-        if abs(point[0] - points_sorted_on_x[mid][0]) < closest_pair_dis:
-            cross_strip.append(point)
-
-    closest_in_strip = dis_between_closest_in_strip(
-        cross_strip, len(cross_strip), closest_pair_dis
-    )
-    return min(closest_pair_dis, closest_in_strip)
-
-
-def closest_pair_of_points(points, points_counts): # call this function only
-    """
-    >>> closest_pair_of_points([(2, 3), (12, 30)], len([(2, 3), (12, 30)]))
-    28.792360097775937
-    """
-    points_sorted_on_x = column_based_sort(points, column=0)
-    points_sorted_on_y = column_based_sort(points, column=1)
-    return (
-        closest_pair_of_points_sqr(
-            points_sorted_on_x, points_sorted_on_y, points_counts
-        )
-    ) ** 0.5
 
 
 # if __name__ == "__main__":
 #     points = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
-#     print("Distance:", closest_pair_of_points(points, len(points)))
+#     closest_pair = closest_pair_of_points(points)
+#     print("Distance:", distance_euclidean(*closest_pair))
