9bc8c7788ce237bcbd334a346a7c88f00df0aa1e
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-08-23 17:16:21 +1000

    Added furthest pair of points function as it utilises convex hull

--- Algorithms Library/Geometry/GrahamScan.py
+++ Algorithms Library/Geometry/GrahamScan.py
@@ -85,7 +85,78 @@ def grahamScanExcludeCollinear(points: list[Point]) -> list[Point]:
     
     return ans
 
+def distance_euclidean(p1: Point, p2: Point) -> float:
+    return ((p1.x-p2.x)**2+(p1.y-p2.y)**2)**0.5
+
+def distance_manhattan(p1: Point, p2: Point) -> float:
+    return abs(p1.x-p2.x) + abs(p1.y-p2.y)
+
+# Currently unneeded
+# def get_signed_triangle_area(p1: Point, p2: Point, p3: Point) -> float:
+#     return abs((p2.x-p1.x)*(p3.y-p2.y)-(p3.x-p2.x)*(p2.y-p1.y)) / 2
+
+def furthest_pair_of_points(points: list[Point], distance_function = distance_euclidean) -> list[Point]:
+    """
+    By constructing a convex hull of points, the furthest pair of points are the vertices that make up the diameter
+    Two pointer method is used to find the diameter
+    """
+    # construct convex hull
+    hull = grahamScanExcludeCollinear(points)
+    n = len(hull)
+    if n == 2:
+        return hull
+    # initialise final distance as the distance between a random point and its antinodal point
+    i1, i2 = 0, 0
+    final_distance = 0
+    point1, point2 = None, None
+    while True:
+        p1, p2 = hull[i1], hull[i2]
+        new_distance = distance_function(p1, p2)
+        if new_distance >= final_distance: # antinodal point may be ahead of i2
+            final_distance = new_distance
+            point1, point2 = p1, p2
+            if i2 == n-1: # last point has been reached
+                break
+            i2 += 1
+        else: # antinodal point has been found at index i2
+            i2 -= 1
+            break 
+    # take the maximum of the rest of the distances between unchecked points and their antinodal points
+    i1 += 1
+    while i1 < n-1:
+        distance = 0
+        while True:
+            p1, p2 = hull[i1], hull[i2]
+            new_distance = distance_function(p1, p2)
+            if new_distance >= distance: # pair with further distance found
+                distance = new_distance
+                if i2 < n-1:
+                    i2 += 1
+                else:
+                    break
+            else:
+                i2 -= 1
+                break
+        if distance > final_distance:
+            final_distance = distance
+            point1, point2 = p1, p2
+        i1 += 1
+    # return the diameter
+    return [point1, point2]
+
 
 # points = [Point(x, y) for x, y in [[0,0],[1,1],[2,2],[3,3],[1,2]]]
 # print(grahamScanIncludeCollinear(points)) # [[0,0],[1,1],[2,2],[3,3],[1,2]]
 # print(grahamScanExcludeCollinear(points)) # [[0,0],[3,3],[1,2]]
+
+# points = [Point(4, 0), Point(0, 2), Point(-1, -7), Point(1, 10), Point(2, -3)]
+# fp = furthest_pair_of_points(points)
+# print(distance_euclidean(*fp)) # 17.11724276862369
+# points = [Point(0, 3), Point(3, 0), Point(0, 0), Point(1, 1)]
+# fp = furthest_pair_of_points(points)
+# print(distance_euclidean(*fp)) # 4.24264
+
+# points = [Point(0, 0), Point(0, 3), Point(3, 3), Point(1, 1), Point(3, 0), Point(1, -3)]
+# fp = furthest_pair_of_points(points, distance_manhattan)
+# print(fp)
+# print(distance_manhattan(*fp)) # 7
